c6fe53bc980d4515112a1e1186f4fb53bbdd48cb
Nivea Stelmam <mand458@hotmail.com>
2025-02-08 16:24:43 -0300

    tests in the application class

---
+++ src/main/java/io/github/niveastelmam/springarchitecture/Application.java
@@ -0,0 +1,35 @@
+package io.github.niveastelmam.springarchitecture;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+
+		//SpringApplication.run(Application.class, args);
+
+		// Another way to start the application
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
+
+		builder.bannerMode(Banner.Mode.OFF); // disable Spring banner
+		builder.profiles("producer", "hml"); // activate a development profile
+		builder.run(args);
+
+
+		ConfigurableApplicationContext applicationContext = builder.context();
+
+		//var produtoRepository = applicationContext.getBean("productRepository");
+
+		ConfigurableEnvironment environment = applicationContext.getEnvironment();
+		String applicationName = environment.getProperty("spring.application.name");
+		System.out.println("Nome da aplicação: " + applicationName);
+
+	}
+
+}
