dbac8ba83635ec7d83dd014928cd72e6ec6b48d1
Nivea Stelmam <mand458@hotmail.com>
2025-02-14 15:46:24 -0300

    bean scope

--- src/main/java/io/github/niveastelmam/springarchitecture/ManagedBean.java
+++ src/main/java/io/github/niveastelmam/springarchitecture/ManagedBean.java
@@ -3,9 +3,17 @@ package io.github.niveastelmam.springarchitecture;
 import io.github.niveastelmam.springarchitecture.todolist.TodoEntity;
 import io.github.niveastelmam.springarchitecture.todolist.TodoValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
 
 @Component
+// Bean scope types
+@Scope(BeanDefinition.SCOPE_SINGLETON) // (default) - does not keep the state
+//@Scope("request") // não guarda estado ----> @Scope(WebApplicationContext.SCOPE_REQUEST)
+//@Scope("session") // single user session - keep the state ----> @Scope(WebApplicationContext.SCOPE_SESSION)
+//@Scope("application") // (specific for web) - all users session - keep the state ----> @Scope(WebApplicationContext.SCOPE_APPLICATION)
 public class ManagedBean {
 
     // injection by constructor
